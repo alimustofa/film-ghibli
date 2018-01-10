06b5d19e8e36f098ffb4a7d27a232be00c3b3b20
Ali <alimustofa97@gmail.com>
2018-01-11 01:43:17 +0700

    Try router history

--- src/index.js
+++ src/index.js
@@ -1,17 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {
-    BrowserRouter as Router,
+    Router,
     Route,
     Switch
 } from 'react-router-dom';
+import createBrowserHistory from 'history/createBrowserHistory';
 import './index.css';
 import App from './App';
 import Detail from './Detail';
 import registerServiceWorker from './registerServiceWorker';
 
+const customHistory = createBrowserHistory();
+
 ReactDOM.render((
-    <Router>
+    <Router history={customHistory}>
         <Switch>
             <Route exact path={`${process.env.PUBLIC_URL}/`} component={App} />
             <Route path={`${process.env.PUBLIC_URL}/film/:id`} component={Detail} />
